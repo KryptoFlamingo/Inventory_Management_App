b0c3973b44a57058f0044a8ecfd6acddbd8e941f
Kirsty Gordon <kirstygordonpd@gmail.com>
2020-11-09 23:25:37 +0000

    manager UI page WIP, working on form requets for stock qty updates

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
+from models.menu import Menu
 from flask import Flask, render_template
+import flask
 
 app = Flask(__name__)
 
@@ -8,3 +10,13 @@ def home():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+@app.route('/manager', methods=['POST', 'GET'])
+def manager():
+    if flask.request.method == 'POST':
+        stock = Menu()
+        stock.meal_beef = flask.request.form
+        # stock.meal_fish = flask.request.form['fish_meal']
+        stock.meal_chicken = flask.request.form
+    return render_template('manager.html')
