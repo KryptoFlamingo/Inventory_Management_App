c670158428ed95a9d947380d386a6fb19c2266a4
Kirsty Gordon <kirstygordonpd@gmail.com>
2020-11-12 16:12:06 +0000

    firt pass at financial functions that feed into the manager page as text for all meal instances.

--- controllers/manager_controller.py
+++ controllers/manager_controller.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, Blueprint
+from psycopg2.extras import DictConnection
 from repositories import meal_repository
 from models.meal import Meal
 from controllers.meal_controller import *
@@ -11,7 +12,9 @@ manager_blueprint = Blueprint("manager", __name__)
 @manager_blueprint.route('/manager', methods=['POST', 'GET'])
 def show_meals():
     meals = meal_repository.select_all()
-    return render_template('manager.html', meals = meals)
+    data = financials(meals)
+    print(data)
+    return render_template('manager.html', meals = meals, data = data)
 
 
 # EDIT
@@ -64,3 +67,44 @@ def new_meal():
 def delete_meal(id):
     manager_repository.delete(id)
     return redirect('/manager')
+
+
+
+
+### FUNCTIONS TO CALCULATE FINANCIAL METRICS BASED ON MEALS SQL TABLE CONTENT
+def financials(meals):
+    list = []
+    data = {}
+    running_total = 0
+
+    for meal in meals:
+# FORECASTS
+# TOTAL FORECAST SALES/
+        full_stock_qty = meal.qty_available + meal.qty_sold
+        total_forecast_sales = meal.selling_price * full_stock_qty
+        # running_total = running_total + total_forecast_sales_for_meal
+
+# TOTAL FORECAST PROFIT/
+        total_forecast_profit = total_forecast_sales - (meal.cost_price * full_stock_qty)
+        # running_total = running_total + total_forecast_profit
+
+        data = {
+            'meal_id' : meal.id,
+            'total_forecast_sales' : total_forecast_sales,
+            'total_forecast_profit' : total_forecast_profit
+        }
+        list.append(data)
+    return list
+
+# # ACTUALS
+#         total_actual_sales = meal.selling_price * meal.qty_sold
+#         data[meal.id].append(total_actual_sales)
+#         profit_per_meal = meal.selling_price - meal.cost_price
+#         data[meal.id].append(profit_per_meal)
+#         total_actual_profit = profit_per_meal * meal.qty_sold
+#         data[meal.id].append(total_actual_profit)
+    data["total_forecast_sales"] = running_total
+    data["total_forecast_sales"] = running_total
+    return data
+
+
--- models/meal.py
+++ models/meal.py
@@ -8,17 +8,5 @@ class Meal:
         self.qty_sold = qty_sold
         self.id = id
 
-
-    # def __init__( self, meal_beef = False, meal_pasta = True, id = None ):
-    #     self.meal_beef = meal_beef
-    #     self.meal_pasta = []
-    #     self.id = id
-
-
 # need to add a meal cost to link with the user wallet and pay for 
 # food - will add later once the rest is sorted
-
-
-# qty in stock for each meal needs handelled somewhere
-
-
